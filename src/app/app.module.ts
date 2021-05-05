4c5b7320d4f403ac870ec267f4779a0320079ebe
loanpas <loanpas@gmail.com>
2021-05-04 22:20:31 -0400

    fix: cambios varios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MasterpersonalComponent } from './pages/masterpersonal/masterpersonal.component';
+import { ComponentsComponent } from './pages/components/components.component';
+import { NominasComponent } from './pages/nominas/nominas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterpersonalComponent,
+    ComponentsComponent,
+    NominasComponent,
   ],
   imports: [
     BrowserModule,
